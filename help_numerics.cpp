3693e93f605f47e0b27ee844d09e8ee85db44f98
fritzmerwyn <felix.m.keil@physik.uni-giessen.de>
2019-11-28 20:02:35 +0100

    Added Location-, Read-in- and Interpolation-Function.
    The Location Function 'locate' was taken from the Numerical Recipes in C 
    and modfied. ATTENTION there still might be an error of giving back the 
    right index.
    
    The 'read-in-data' Function reads data from a file that is passed to it 
    ("Data/LOOKUPTABLE1.dat" eg.). ATTENTION there still might be an error 
    of giving back the right index.
    
    The 'bilinearinterpol' Function is also taken from the Numerical Recipes 
    in C - Book. Instructions/Manual will follow!

--- help_numerics.cpp
+++ help_numerics.cpp
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <iostream>
 #include "help_numerics.h"
+#include <complex>
+#include <fstream>
 #define EPS 3.0e-11 // EPS is the relative precision.
 
 double qgaus1(double (*func)(double), double* x, double* w){
@@ -63,7 +65,7 @@ for(i=1;i<=m;i++) {
 
   for(int k = 0;k<n; k++){
     x[k] = x[k+1]; // WARNING the last entry (n+1) is still there but is the same as n. Make sure the other programs do nut run until the last entry <=n but only <n!
-    w[k] = w[k+1]; // WARNING the last entry (n+1) is still there but is the same as n. Make sure the other programs do nut run until the last entry <=n but only <n! 
+    w[k] = w[k+1]; // WARNING the last entry (n+1) is still there but is the same as n. Make sure the other programs do nut run until the last entry <=n but only <n!
   }
 
 
@@ -79,4 +81,93 @@ for(i=1;i<=m;i++) {
   abcissas_gl2d[1] = w;
 
   return abcissas_gl2d;
+}
+
+int locate(double* xx, double x, int length){
+
+    unsigned long ju,jm,jl;
+    int ascnd;
+
+    if(x == xx[0]) return 0;
+    else if(x == xx[length-1]) return length-2;
+
+    jl=0;
+    ju=length;
+    ascnd=(xx[length-1] >= xx[0]);
+    while(ju-jl > 1){
+      // jm=(ju+jl) >> 1; // original code... shifts the bit value of jm one bit to right eg from 6=00110 to 3=00011
+      jm=(ju+jl)/2; // original code... shifts the bit value of jm one bit to right eg from 6=00110 to 3=00011
+
+      if (x >= xx[jm] == ascnd){
+        jl=jm;
+      }
+      else{
+        ju=jm;
+      }
+    }
+
+  return jl;
+}
+
+void read_in_data(const std::string filename, double* q_vec, double* z_vec, std::complex<double>*** y_vals){
+  std::complex<double> Imag = {0.0,1.0};
+  double x0,x1,x2,x3,x4,x5;
+  int i,j,k;
+  i=0;
+  j=0;
+  k=0;
+  std::ifstream inputfile(filename);
+  inputfile.ignore(50, '\n');
+
+  if (!inputfile) {
+    std::cerr << "Unable to open specified file";
+    exit(1);   // call system to stop
+  }
+
+  while (inputfile >> x0 >> x1 >> x2 >> x3) {
+
+      if(k == 0){
+        q_vec[j] = x0;
+      }
+      if(j == 0){
+        z_vec[i] = x1;
+      }
+
+      y_vals[0][j][k] = x2 + Imag*x3;
+      // y_vals[1][j][k] = x4 + Imag*x5;
+
+      i++;
+      k++;
+      if(k == ang_absciss_points){
+        k=0;
+        j++;
+      }
+  }
+  inputfile.close();
+
+}
+
+double* get_qz(std::complex<double> x0, double m_pion, double routing_plus){
+  double q = std::sqrt(x0.real() + routing_plus * routing_plus * m_pion * m_pion);
+  double z = (x0.imag())/(routing_plus * m_pion * q);
+
+  double* got_qz = nullptr;
+  got_qz = new double[2];
+  got_qz[0] = q;
+  got_qz[1] = z;
+
+  return got_qz;
+}
+
+std::complex<double> bilinearinterpol(std::complex<double> x0, double* x_corners, std::complex<double>* y_corners, double routing_plus, double m_pion){
+  double t,u;
+
+  double* qz = nullptr;
+
+  qz = get_qz(x0,m_pion,routing_plus);
+
+  t = (qz[0] - x_corners[0])/(x_corners[1]-x_corners[0]);
+  u = (qz[1] - x_corners[2])/(x_corners[3]-x_corners[2]);
+
+  return (1-t)*(1-u)*y_corners[0] + t*(1-u)*y_corners[1] + t*u*y_corners[2] + (1-t)*u*y_corners[3];
 }
